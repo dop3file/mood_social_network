6c50d81ea87ca987433527cf2815043e780619b8
dop3file <49581209+dop3file@users.noreply.github.com>
2022-08-21 05:43:56 +0300

    bug fix
    buf fix with validate image

--- mood/core/utils.py
+++ mood/core/utils.py
@@ -1,6 +1,10 @@
+import os
+
+
 def validate_profile_avatar(instance, file_name):
     if len(file_name.split('.')) <= 1:
         raise ValueError
-    if file_name.split('.')[1].lower() not in ['jpg', 'png', 'gif']:
+    if file_name.split('.')[-1].lower() not in ['jpg', 'png', 'gif']:
         raise ValueError
+    
     return f'images/{instance.user.username}.{file_name.split(".")[1]}'
