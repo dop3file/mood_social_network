0c36886d249c9d04166766fa41f4afa334b3206f
dop3file <49581209+dop3file@users.noreply.github.com>
2022-08-21 14:52:31 +0300

    0.9
    add deleting old avatar from server when user upload new avatar

--- mood/core/utils.py
+++ mood/core/utils.py
@@ -1,10 +1,23 @@
 import os
 
 
+ALL_EXTENSIONS = ['jpg', 'png', 'gif', 'webp']
+
+
+def delete_old_avatar(instance, file_name):
+    for extension in ALL_EXTENSIONS:
+        try:
+            os.remove(f'media/images/{instance.user.username}.{extension}')
+        except FileNotFoundError:
+            pass
+
+
 def validate_profile_avatar(instance, file_name):
     if len(file_name.split('.')) <= 1:
         raise ValueError
-    if file_name.split('.')[-1].lower() not in ['jpg', 'png', 'gif']:
+    if file_name.split('.')[-1].lower() not in ALL_EXTENSIONS:
         raise ValueError
     
-    return f'images/{instance.user.username}.{file_name.split(".")[1]}'
+    
+    delete_old_avatar(instance, file_name)
+    return f'images/{instance.user.username}.{file_name.split(".")[-1]}'
