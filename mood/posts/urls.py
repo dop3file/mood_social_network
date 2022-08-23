cde9ee356e47e0010e7d604a0f8ffd8b5bd4c874
dop3file <49581209+dop3file@users.noreply.github.com>
2022-08-23 04:32:41 +0300

    1.4
    add all posts page with paginator

--- mood/posts/urls.py
+++ mood/posts/urls.py
@@ -4,5 +4,6 @@ from posts import views
 
 urlpatterns = [
     path('add_post/', views.add_post, name='add_post'),
-    path('delete_post/<int:post_id>', views.delete_post, name='delete_post')
+    path('delete_post/<int:post_id>', views.delete_post, name='delete_post'),
+    path('all_posts/<slug:username>/<int:index_page>', views.get_all_posts, name='all_posts')
 ]
--- mood/posts/views.py
+++ mood/posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.contrib.auth.decorators import login_required   
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
 from .forms import PostForm
 from posts.models import Post
+from core.models import Profile
 
 
 def add_post(request):
@@ -25,10 +28,26 @@ def add_post(request):
 
 
 def delete_post(request, post_id):
-    post = Post.objects.get(id=post_id)
+    try:
+        post = Post.objects.get(id=post_id)
+    except ObjectDoesNotExist:
+        return HttpResponseBadRequest('status code - 404')
     if post.user_id == request.user.id:
         post.delete()
         return redirect('profile', username=request.user.username)
     else:
         return redirect('index') 
         
+
+def get_all_posts(request, username, index_page):
+    context = {}
+    try:
+        all_posts = Paginator(Post.objects.filter(user__username=username).order_by('-date_post').all(), 5)
+        context['all_posts'] = all_posts.page(index_page)
+        context['profile'] = Profile.objects.get(user__username=username)
+        context['user'] = context['profile'].user
+        
+    except ObjectDoesNotExist:
+        return HttpResponseBadRequest('status code - 404')
+    
+    return render(request, 'all_posts.html', context=context)
